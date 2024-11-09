fd7092a1fef77df6f76474b913a54589136c1c17
Charifx7 <charif.edu7@gmail.com>
2024-11-09 16:53:42 +0700

    initial commit

---
+++ src/components/ContactSection.vue
@@ -0,0 +1,18 @@
+<template>
+    <section class="text-white mt-20" id="contact">
+        <h2 class="text-4xl font-bold text-white text-center mb-4 px-4 xl:pl-16">Contact</h2>
+        <div class="" data-aos="zoom-in-up">
+            <div class="text-center text-3xl pl-10 mt-10">Email : charif.edu7@gmail.com</div>
+            <div class="text-center text-3xl pl-10 mt-10">Tel : 0973488899</div>
+        </div>
+        <div
+            class="py-8 xl:px-16 px-4 sm:py-16 grid grid-cols-1 gap-6 pt-10 sm:grid-cols-2 md:gap-10 md:pt-12 lg:grid-cols-3">
+        </div>
+        <div
+            class="py-8 xl:px-16 px-4 sm:py-16 grid grid-cols-1 gap-6 pt-10 sm:grid-cols-2 md:gap-10 md:pt-12 lg:grid-cols-3">
+        </div>
+    </section>
+</template>
+<script>
+
+</script>
