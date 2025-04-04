c0ee3645ffa778fd078312236b02ce31ffb88ace
Charifx7 <charif.edu7@gmail.com>
2025-04-04 13:36:23 +0700

    delete docker

--- src/components/ExperienceAndSkills.vue
+++ src/components/ExperienceAndSkills.vue
@@ -2,10 +2,10 @@
     <section class="text-white mt-18" id="skills">
         <div class="absolute right-0 top-[110rem] h-full w-full justify-end">
         </div>
-        <div class="md:grid md:grid-cols-2 gap-8 items-center py-1 px-4 xl:gap-16 xl:px-16">
+        <div class="md:grid md:grid-cols-2 gap-8 items-center py-1 px-4 xl:gap-16 xl:px-16 text-center ">
             <div class="mt-4 md:mt-0 text-left flex flex-col z-10 h-full w-[80%]">
                 <h2 class="text-4xl font-bold text-white text-left mb-4">
-                    <span class="text-cyan-300 pt-8 text-center lg:text-left mx-auto max-w-xl text-4xl">Skills</span>
+                    <span class="text-cyan-300 pt-8 text-center lg:text-left mx-auto max-w-xl text-4xl ">Skills</span>
                 </h2>
                 <div class="mt-8" v-for="skill in Skills" :key="skill.id">
                     <div class="flex items-end justify-between" data-aos="fade-right">
@@ -17,8 +17,8 @@
                 </div>
             </div>
             <div class="mt-4 md:mt-0 text-left flex flex-col z-10 h-full w-[80%]">
-                <h2 class="text-4xl font-bold text-white text-left mb-4">
-                    <span class="text-cyan-300 pt-8 text-center lg:text-left mx-auto max-w-xl text-4xl">Tools</span>
+                <h2 class="text-4xl font-bold text-white text-left mb-4 ">
+                    <span class="text-cyan-300 pt-8 text-center lg:text-left mx-auto max-w-xl text-4xl ">Tools</span>
                 </h2>
                 <div class="mt-8" v-for="tool in Tools" :key="tool.id">
                     <div class="flex items-end justify-between" data-aos="fade-right">
@@ -36,12 +36,12 @@ import { ref } from 'vue';
 const Skills = ref([
     {
         id: 1,
-        name: 'HTML & CSS',
+        name: 'HTML',
         level: 'Advanced'
     },
     {
         id: 2,
-        name: 'Tailwind CSS',
+        name: 'CSS, Tailwind CSS',
         level: 'Beginner'
     },
     {
@@ -76,12 +76,8 @@ const Tools = ref([
         name: 'Postman',
     },
     {
-        id: 3,
-        name: 'Figma',
-    },
-    {
-        id: 3,
-        name: 'Photoshop',
+        id: 4,
+        name: 'Docker',
     },
 
 ]);
