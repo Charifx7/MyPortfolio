092f12099b707eec5205922ab4563e96e1349a3c
Charifx7 <charif.edu7@gmail.com>
2025-06-19 12:22:12 +0700

    update projsection

--- src/components/LatestProjSection.vue
+++ src/components/LatestProjSection.vue
@@ -59,25 +59,7 @@ const Projects = ref([
         gitURL: 'https://github.com/Charifx7/fullstack_MEVN_recipe_app',
         webURL: 'https://fullstack-mevn-recipe-app-charif.vercel.app/'
     },
-    {
-        id: 2,
-        image: '/assets/project1.png',
-        title: 'To-do list',
-        description: 'To-do list project with Mock API using Pinia. Features include task creation, editing, deletion, and filtering.',
-        technologies: ['Vue', 'Pinia', 'Mock-API', 'TailwindCSS'],
-        gitURL: 'https://github.com/Charifx7/todo-list',
-        webURL: 'https://todo-list-beta-two-62.vercel.app/'
-    },
-    {
-        id: 3,
-        image: '/assets/project2.png',
-        title: 'E-Commerce with Temporary Data Storage',
-        description: 'E-commerce front-end project with temporary data storage using Pinia. Features include product display, cart management.', 
-        technologies: ['Vue', 'TailwindCSS', 'Pinia'],
-        gitURL: 'https://github.com/Charifx7/e-commerce-front-end',
-        webURL: 'https://e-commerce-front-end-fawn.vercel.app/'
-    },
-   
+
     {
         id: 4,
         image: '/assets/project0.jpg',
